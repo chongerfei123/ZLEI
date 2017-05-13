5891688e06abb5c23b1447f64e8840c1a5bc409e
chongerfei123 <779545095@qq.com>
2017-05-13 20:16:40 +0800

    把groupButton换成了VIewpager+tablayout

--- app/src/main/java/com/example/zl/zlei/View/MainActivity.java
+++ app/src/main/java/com/example/zl/zlei/View/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.zl.zlei.View;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.animation.TranslateAnimation;
-import android.widget.FrameLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.support.v4.view.ViewPager;
 
 import com.example.zl.zlei.Present.MainPresent;
 import com.example.zl.zlei.R;
@@ -15,58 +11,28 @@ import com.example.zl.zlei.View.activi.BaseActivity;
 import com.example.zl.zlei.View.activi.MainActivityInterface;
 import com.example.zl.zlei.View.frg.JokesFragment;
 import com.example.zl.zlei.View.frg.NewsFragment;
-import com.example.zl.zlei.View.frg.channalfrg.TopFragment;
-import com.example.zl.zlei.listener.OnScrollListener;
+import com.example.zl.zlei.adapter.MainPagerAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity<MainActivityInterface, MainPresent> implements MainActivityInterface {
 
-    @BindView(R.id.radioButton_news)
-    RadioButton radioButtonNews;
-    @BindView(R.id.radioButton_jokes)
-    RadioButton radioButtonJokes;
-    @BindView(R.id.frameLayout_fragment)
-    FrameLayout frameLayout_fragment;
-    @BindView(R.id.radioGroup)
-    RadioGroup radioGroup;
-
-    private NewsFragment newsFragment;
-    private JokesFragment jokesFragment;
-    private FragmentManager fragmentManager;
+    @BindView(R.id.main_tab)
+    TabLayout mainTab;
+    @BindView(R.id.main_viewPager)
+    ViewPager mainViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        FragmentInit();
-    }
-
-    @OnClick(R.id.radioButton_news)
-    public void radioButtonNewsOnClick() {
-        Toast.makeText(this, "radioButtonNewsOnClick", Toast.LENGTH_SHORT).show();
-        mPresenter.changeFragment(newsFragment, fragmentManager);
-    }
-
-    @OnClick(R.id.radioButton_jokes)
-    public void radioButtonJokesOnClick() {
-        Toast.makeText(this, "radioButtonJokesOnClick", Toast.LENGTH_SHORT).show();
-        mPresenter.changeFragment(jokesFragment, fragmentManager);
-    }
-
-
-    private void FragmentInit() {
-        newsFragment = new NewsFragment();
-        jokesFragment = new JokesFragment();
-        fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.frameLayout_fragment, newsFragment, "news");
-        transaction.add(R.id.frameLayout_fragment, jokesFragment, "jokes");
-        transaction.hide(jokesFragment);
-        transaction.commit();
+        FragmentManager fm = getSupportFragmentManager();
+        MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(fm);
+        mainViewPager.setAdapter(mainPagerAdapter);
+        mainTab.setupWithViewPager(mainViewPager);
+        //mainViewPager.setScrollIndicators(null);
     }
 
     @Override
